1f156d0c56b3d2680bcec1119de8660b315efc5a
Marco Carola <marco.carola1@merck.com>
2017-09-21 09:57:00 +0100

    - Added Excchanges Interface and Skeleton classes - Added "CheckEmail" skeleton class

---
+++ AlertTrader/APIExchanges/BitfinexExchange.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlertTrader.APIExchanges
+{
+    public class BitfinexExchange : IAPIExchange
+    {
+        public void CloseAllpositions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetBalance(string symbol)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetCurrentPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InitializeExchange()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Long()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Short()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AlertTrader/Classes/EmailChecker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlertTrader
+{
+    public class EmailChecker
+    {
+        public EmailChecker(string username, string password)
+        {
+
+        }
+
+        public string CheckEmail()
+        {
+            return string.Empty;
+        }
+    }
+}
